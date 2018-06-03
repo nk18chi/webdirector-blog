7e88ab84b305d0f5f214cd8ca2d8fa96b1771ee2
naoki mita <naoki.mita18@gmail.com>
2018-06-03 15:03:54 +0900

    imagepathの変更 sp

--- blog/models.py
+++ blog/models.py
@@ -3,35 +3,6 @@ from datetime import datetime
 from django.db import models
 from django.conf import settings
 
-def delete_previous_file(function):
-    """不要となる古いファイルを削除する為のデコレータ実装.
-
-    :param function: メイン関数
-    :return: wrapper
-    """
-    def wrapper(*args, **kwargs):
-        """Wrapper 関数.
-
-        :param args: 任意の引数
-        :param kwargs: 任意のキーワード引数
-        :return: メイン関数実行結果
-        """
-        self = args[0]
-
-        # 保存前のファイル名を取得
-        result = Image.objects.filter(pk=self.pk)
-        previous = result[0] if len(result) else None
-        super(Image, self).save()
-
-        # 関数実行
-        result = function(*args, **kwargs)
-
-        # 保存前のファイルがあったら削除
-        if previous:
-            os.remove(previous.image.name)
-        return result
-    return wrapper
-
 
 class BlogCategory(models.Model):
     name = models.CharField(max_length=200)
@@ -43,14 +14,6 @@ class BlogCategory(models.Model):
 
 
 class Image(models.Model):
-    @delete_previous_file
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        super(Image, self).save()
-
-    @delete_previous_file
-    def delete(self, using=None, keep_parents=False):
-        super(Image, self).delete()
-
     name = models.CharField(max_length=50)
     upload_time = models.DateTimeField(default=datetime.now)
     image = models.ImageField(upload_to=settings.MEDIA_URL, null=True, blank=True)
