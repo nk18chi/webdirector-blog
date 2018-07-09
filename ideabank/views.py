aa2ea50bd86934136696b96a4bb13754a4a69568
naoki mita <naoki.mita18@gmail.com>
2018-07-09 21:55:55 +0900

    apiにpermission設定

--- ideabank/views.py
+++ ideabank/views.py
@@ -2,6 +2,7 @@
 
 import django_filters
 from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Idea
 from .serializer import IdeaSerializer
 from random import sample
@@ -10,4 +11,6 @@ class IdeaViewSet(viewsets.ModelViewSet):
     queryset = Idea.objects.filter(
         status__exact=1,
     ).order_by('?')[:3]
+    
     serializer_class = IdeaSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
