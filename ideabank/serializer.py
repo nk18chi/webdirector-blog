99cd4366d922a05aa1648f7d5b43d78886f4e08c
Naoki Mita <naoki.mita18@gmail.com>
2019-09-27 13:56:54 -0700

    delete ideabank / can display image in local / refresh gitignore

--- ideabank/serializer.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-from rest_framework import serializers
-from .models import Idea
-
-class IdeaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Idea
-        fields = ('id', 'text', 'created_at', 'status')
--- webdirectorblog/urls.py
+++ webdirectorblog/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from ideabank.urls import router as ideabank_router
 
 urlpatterns = [
     url(r'^nk-admin/', admin.site.urls),
-    url(r'^api/', include(ideabank_router.urls)),
     url(r'^', include('blog.urls', namespace='blog')),
     url('markdownx/', include('markdownx.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
