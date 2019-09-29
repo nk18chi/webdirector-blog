7fdb19ceb82305af816ffe3ed8985d3e79445f7e
Naoki Mita <naoki.mita18@gmail.com>
2019-09-29 16:56:04 -0700

    you can get blog post data from api

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,9 +1,43 @@
 from rest_framework import serializers
 
-from blog.models import BlogPost
+from blog.models import BlogPost, Image, BlogTag
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = (
+            'id',
+            'name',
+            'image',
+        )
+
+
+class BlogTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogTag
+        fields = (
+            'id',
+            'name',
+        )
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
+    image_square = ImageSerializer(read_only=True)
+    blog_tag = BlogTagSerializer(read_only=True, many=True)
+    seo_description = serializers.SerializerMethodField()
+
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        fields = (
+            'id',
+            'title',
+            'seo_description',
+            'created_at',
+            'category',
+            'image_square',
+            'blog_tag'
+        )
+
+    def get_seo_description(self, obj):
+        return obj.seo_description[:140]
