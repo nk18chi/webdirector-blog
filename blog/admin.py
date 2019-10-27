bf9424b2a0d2d23eb18238a4703f7bdace2fbf4b
Naoki Mita <naoki.mita18@gmail.com>
2019-10-27 16:16:56 -0700

    Updated admin for using modeltranslation

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import BlogPost, BlogCategory, BlogTag, Image
-from markdownx.admin import MarkdownxModelAdmin
+from modeltranslation.admin import TranslationAdmin
 
-
-class BlogPostAdmin(MarkdownxModelAdmin):
+class BlogPostAdmin(TranslationAdmin):
     list_display = ('id', 'title', 'status', 'created_at',
                     'category')
     list_display_links = ('id', 'title')
-
 
 admin.site.register(BlogPost, BlogPostAdmin)
 
@@ -16,21 +14,19 @@ class ImageAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'image')
     list_display_links = ('id', 'name')
 
-
 admin.site.register(Image, ImageAdmin)
 
 
-class BlogCategoryAdmin(admin.ModelAdmin):
+class BlogCategoryAdmin(TranslationAdmin):
+# class BlogCategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'order')
     list_display_links = ('id', 'name')
-
 
 admin.site.register(BlogCategory, BlogCategoryAdmin)
 
 
-class BlogTagAdmin(admin.ModelAdmin):
+class BlogTagAdmin(TranslationAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
-
 
 admin.site.register(BlogTag, BlogTagAdmin)
