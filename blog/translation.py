aa933b7d0380aa9c8892c2aca071f1a5bfdc6163
Naoki Mita <naoki.mita18@gmail.com>
2019-10-27 16:00:41 -0700

    Created migration files which is related to blog model

--- blog/translation.py
+++ blog/translation.py
@@ -1,8 +1,16 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import BlogPost
+from .models import BlogPost, BlogCategory, BlogTag
 
 class BlogPostTranslationOptions(TranslationOptions):
-    fields = ('title', 'content', 'status')
+    fields = ('title', 'content', 'seo_description', 'status')
+
+class BlogCategoryTranslationOptions(TranslationOptions):
+    fields = ('name', 'description')
+
+class BlogTagTranslationOptions(TranslationOptions):
+    fields = ('name', 'description')
 
 translator.register(BlogPost, BlogPostTranslationOptions)
+translator.register(BlogCategory, BlogCategoryTranslationOptions)
+translator.register(BlogTag, BlogCategoryTranslationOptions)
 
