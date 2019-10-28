36c1e6b9ee813aa24afc0639fccd3d0ed353a022
Naoki Mita <naoki.mita18@gmail.com>
2019-10-28 14:06:00 -0700

    Displayed english fields in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,9 @@ from .models import BlogPost, BlogCategory, BlogTag, Image
 from modeltranslation.admin import TranslationAdmin
 
 class BlogPostAdmin(TranslationAdmin):
-    list_display = ('id', 'title', 'status', 'created_at',
+    list_display = ('id', 'title', 'title_en', 'status', 'status_en', 'created_at',
                     'category')
-    list_display_links = ('id', 'title')
+    list_display_links = ('id',)
 
 admin.site.register(BlogPost, BlogPostAdmin)
 
@@ -19,14 +19,14 @@ admin.site.register(Image, ImageAdmin)
 
 class BlogCategoryAdmin(TranslationAdmin):
 # class BlogCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'order')
-    list_display_links = ('id', 'name')
+    list_display = ('id', 'name', 'name_en', 'order')
+    list_display_links = ('id',)
 
 admin.site.register(BlogCategory, BlogCategoryAdmin)
 
 
 class BlogTagAdmin(TranslationAdmin):
-    list_display = ('id', 'name')
-    list_display_links = ('id', 'name')
+    list_display = ('id', 'name', 'name_en')
+    list_display_links = ('id',)
 
 admin.site.register(BlogTag, BlogTagAdmin)
